a8f34b69aa4158a6fd6a092e7a848d74572e7ef6
Aalajlan6 <aalajlan6@gatech.edu>
2024-06-12 08:25:58 -0400

    added

--- bio_testing.py
+++ bio_testing.py
@@ -2,7 +2,8 @@ from Bio import Entrez
 from Bio import SeqIO
 
 Entrez.email = "aalajlan6@gatech.edu"  # Change it to your email address
+gene_id = "3300066319 assembled Ga0610651_106470_1_93"
 handle = Entrez.efetch(db="protein", id=gene_id, rettype="gb", retmode="text")
 record = SeqIO.read(handle, "genbank")  # Reading the sequence
 handle.close()
-return gene_id, str(record.seq)  # Returns a tuple with the gene_id and the amino acid sequence
+print(gene_id, str(record.seq))  # Returns a tuple with the gene_id and the amino acid sequence
